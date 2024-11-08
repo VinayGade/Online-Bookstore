88492f3c7e6e8b43a371c82bb509245fe710c7b0
VinayGade <vinay.gaade@gmail.com>
2024-11-08 22:21:18 +0530

    library home page, footer Search Book functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Footer } from './layouts/footer/Footer';
+import { HomePage } from './layouts/HomePage/HomePage';
+import { Navbar } from './layouts/navbar/Navbar';
+import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      {/*<HomePage/>*/}
+      <SearchBooksPage />
+      <Footer/>
     </div>
   );
 }
-
-export default App;
---
+++ src/layouts/HomePage/components/ExploreTopBooks.tsx
@@ -0,0 +1,13 @@
+export const ExploreTopBooks = () => {
+    return(
+        <div className="p-5 mb-4 bg-dark header">
+            <div className='container-fluid py-5 text-white d-flex justify-content-center align-items-center'>
+                <div>
+                    <h1 className='display-5 fw-bold'>Find your next book</h1>
+                    <p className='col-md-8 fs-4'>Which book would you like to read next?</p>
+                    <a href='#' type='button' className='btn main-color btn-lg text-white'>Explore top books</a>
+                </div>
+            </div>
+        </div>
+    )
+}
